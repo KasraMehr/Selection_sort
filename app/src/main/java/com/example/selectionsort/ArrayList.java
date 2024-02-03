8c364a3416885bf0f307b3153f15100b9e249636
Kasra Mehralizadeh <kasramehrali@gmail.com>
2024-02-03 19:21:50 +0330

    Update ArrayList.java

--- app/src/main/java/com/example/selectionsort/ArrayList.java
+++ app/src/main/java/com/example/selectionsort/ArrayList.java
@@ -2,12 +2,15 @@ package com.example.selectionsort;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+
 
 public class ArrayList {
     private int size;
     private List<String> array;
 
-    public ArrayList(int size, List<String> array){
+    public ArrayList(int size, List<String> array)
+    {
         this.size = size;
         this.array = array;
     }
@@ -28,7 +31,8 @@ public class ArrayList {
         return array;
     }
 
-    public List<String> stepSelectionSort(List<String> array, int size, int step){
+    private List<String> stepSelectionSort(List<String> array, int size, int step)
+    {
         int smallest = Integer.parseInt(array.get(step));
         int smallestIndex = step;
         for(int i=step+1; i<size; i++){
@@ -37,21 +41,36 @@ public class ArrayList {
                 smallestIndex = i;
             }
         }
-        int temp = Integer.parseInt(array.get(0));
-        array.set(0, String.valueOf(smallest));
+        int temp = Integer.parseInt(array.get(step));
+        array.set(step, String.valueOf(smallest));
         array.set(smallestIndex, String.valueOf(temp));
         return array;
     }
 
-    public List<List<String>> SelectionSort(List<String> array, int size){
+    public List<List<String>> SelectionSort(List<String> array, int size)
+    {
         List<List<String>> arrays = Collections.singletonList(array);
         int step = 0;
         while(arrays.get(step) != stepSelectionSort(array, size, step)){
-            array = stepSelectionSort(array, size, step);
+            stepSelectionSort(array, size, step);
             step += 1;
             arrays.add(array);
         }
         return arrays;
+    }
+
+    public List<String> createRandomNumbers(int size)
+    {
+        Random rand = new Random();
+        if(size < 1){
+            return Collections.singletonList("Error");
+        }
+        List<String> array = Collections.singletonList(String.valueOf(rand.nextInt(1000)));
+        for(int i = 1; i <= size+1; i++){
+            assert false;
+            array.add(String.valueOf(rand.nextInt(1000)));
+        }
+        return array;
     }
 
 }
